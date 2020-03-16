7262a9972033e1765ebb6635186cb9b269fc61dd
Dexeat <Dexeat@users.noreply.github.com>
2020-03-16 15:26:39 +0100

    v1.3

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var config = {
         default: 'arcade',
         arcade:{
             debug: true,
-            gravity: {y: 6000,x:10000},
+            gravity: {y: 5000,/*x:300*/},
         },
         
     },
@@ -28,11 +28,15 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('bg', 'assets/fond/fond.png');
+    this.load.image('bg1', 'assets/fond/fond1.jpg');
+    this.load.image('bg2', 'assets/fond/fond2.jpg');
     this.load.image('char', 'assets/char/char.png');
-    this.load.image('rock','assets/decor/Edwig.png')
+    this.load.image('rock','assets/decor/rock.png')
     this.load.image('sol','assets/decor/sol.jpg')
-    this.load.image('plateforme', 'assets/decor/plateforme.jpg')
+    this.load.image('p', 'assets/decor/plateforme.jpg')
+    this.load.image('pv', 'assets/decor/plateformev.jpg')
+    this.load.image('pi', 'assets/decor/pi.png')
+    this.load.image('piv', 'assets/decor/piv.png')
 }
 
 function create ()
@@ -44,11 +48,14 @@ function create ()
     rock = this.physics.add.staticGroup();
     sol = this.physics.add.staticGroup();
     platforme = this.physics.add.staticGroup();
-
+    platformev = this.physics.add.staticGroup();
+    platformei = this.physics.add.staticGroup();
+    platformeiv = this.physics.add.staticGroup();
 
 
     //fond
-    fond = this.add.image(0, 0, 'bg').setOrigin(0);
+    fond = this.add.image(0, 0, 'bg1').setOrigin(0);
+    fond2 = this.add.image(1024, 0, 'bg2').setOrigin(0);
     
     cursors = this.input.keyboard.createCursorKeys();
     player = this.physics.add.image(0, 740, 'char');
@@ -59,20 +66,25 @@ function create ()
 
     //ajout des groupes
     sol.create(1024,767, 'sol' )
-    rock.create(64, 750, 'rock');
-    platforme.create(130,670,'plateforme');
-    platforme.create(300,570, 'plateforme');
+    rock.create(20, 727, 'rock').setOrigin(0).refreshBody();
+    platforme.create(130,670,'p').setOrigin(0).refreshBody();
+    platforme.create(300, 570, 'p').setOrigin(0).refreshBody();
+    platformev.create(300,300, 'pv').setOrigin(0).refreshBody();
+    platformev.create(750, 300, 'pv').setOrigin(0).refreshBody();
+    platforme.create(600, 670, 'p').setOrigin(0).refreshBody();
+    platforme.create(800, 570, 'p').setOrigin(0).refreshBody();
 
     //colision
     this.physics.add.collider(player, rock);
     this.physics.add.collider(player, sol);
     this.physics.add.collider(player, platforme);
+    this.physics.add.collider(player, platformev);
 }
 
 function update ()
 {
 
-    player.setVelocity(0);
+    player.setVelocityX(350);
     if (cursors.left.isDown){
         player.setVelocityX(-8000);
     }
@@ -88,7 +100,7 @@ function update ()
     }*/
 
     if (cursors.up.isDown  && player.body.touching.down ) {
-        player.setVelocityY(-9500);
+        player.setVelocityY(-1500);
     }
     if (cursors.down.isDown) {
         player.setVelocityY(500);
